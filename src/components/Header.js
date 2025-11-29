0c66b0e2bf55cfebc77ecaad74a006a2c422b96f
adhya <adhya.bhat@livnsense.com>
2025-11-29 19:10:10 +0530

    added login btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
-  import {logo} from '../utils/constants'
-  const HeaderComponent = () =>(
+import { logo } from '../utils/constants'
+import { useState } from 'react'
+const HeaderComponent = () => {
+    const [btnName, setBtnName] = useState("Login")
+    console.log("Header rendered")
+    return (
+
         <div className='heading'>
             <div className='logo-container'>
-            <img className='logo' src={logo}/>
+                <img className='logo' src={logo} />
             </div>
             <div className='nav-items'>
                 <ul>
@@ -11,9 +16,11 @@
                     <li>Contact</li>
                     <li>Cart</li>
                 </ul>
+                <button className='login' onClick={() => setBtnName(btnName == "Login" ? "Logout" : "Login") 
+                  }>{btnName}</button>
 
-            </div>
         </div>
+        </div >
 
-    )
-    export default HeaderComponent;
+    )}
+export default HeaderComponent;
